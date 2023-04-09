37ec617d158c0f43a760db8666b53fb71bf2f8ce
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2023-04-09 21:46:18 +0530

    worked with login route

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helper/authHelper.js";
+import { comparePassword, hashPassword } from "../helper/authHelper.js";
+import JWT from "jsonwebtoken";
+import colors from "colors";
 
 export const registerController = async (req, res) => {
   try {
@@ -45,6 +47,60 @@ export const registerController = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Error occur while registering you",
+      error,
+    });
+  }
+};
+
+export const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password)
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+
+    //  check user
+    const user = await userModel.findOne({ email });
+    if (!user)
+      return res.status(401).json({
+        success: false,
+        message: "The email is not registered",
+      });
+
+    // check for password
+    const match = await comparePassword(password, user.password);
+    if (!match)
+      return res.status(200).json({
+        success: false,
+        message: "Invalid password",
+      });
+
+    // creating token
+    const token = await JWT.sign({ _id: user._id }, process.env.JWT_KEY, {
+      expiresIn: "1d",
+    });
+
+    // sending the response
+    res.status(200).json({
+      success: true,
+      message: "Successfully login",
+      user: {
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        address: user.address,
+      },
+      token,
+    });
+  } catch (error) {
+    console.log("Error occur while log in the user ".bgRed.white);
+    res.status(500).json({
+      success: false,
+      message: "There is something wrong with Login",
       error,
     });
   }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,5 +1,8 @@
 import express from "express";
-import { registerController } from "../controllers/authController.js";
+import {
+  registerController,
+  loginController,
+} from "../controllers/authController.js";
 // creating router object
 const router = express.Router();
 
@@ -7,5 +10,8 @@ const router = express.Router();
 
 // 1. Register || Post method
 router.post("/register", registerController);
+
+// 2.Login || method post
+router.post("/login", loginController);
 
 export default router;
